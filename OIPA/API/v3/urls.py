456c1300af85906f67ba728620c4940c8f19b00b
Vincent van 't Westende <vincent@zimmermanzimmerman.nl>
2013-09-24 13:42:00 +0200

    url fix try 1

--- OIPA/API/v3/urls.py
+++ OIPA/API/v3/urls.py
@@ -6,6 +6,8 @@ from tastypie.api import Api
 from API.v3.resources.model_resources import *
 from API.v3.resources.advanced_resources import *
 from API.v3.resources.activity_view_resources import *
+from API.v2 import views as old_views
+from API.v3 import views
 
 from API.v2.urls import v2_api;
 
@@ -36,16 +38,16 @@ urlpatterns = patterns('',
     (r'^v3/$', api_v3_docs),
     url(r'^v2/docs/$', 'API.v2.views.docs_index', name='docsv2'),
     url(r'^v2/docs/getting-started/$', 'API.v2.views.docs_start', name='start_docsv2'),
-    url(r'^v2/docs/resources/$', 'API.v2.views.docs_resources', name='resource_docsv2'),
-    url(r'^v2/docs/filtering/$', 'API.v2.views.docs_filtering', name='filter_docsv2'),
-    url(r'^v2/docs/ordering/$', 'API.v2.views.docs_ordering', name='ordering_docsv2'),
-    url(r'^v2/docs/about/$', 'API.v2.views.docs_about', name='about_docsv2'),
-    url(r'^v3/docs/$', 'API.v3.views.docs_index', name='docs'),
-    url(r'^v3/docs/getting-started/$', 'API.v3.views.docs_start', name='start_docs'),
-    url(r'^v3/docs/resources/$', 'API.v3.views.docs_resources', name='resource_docs'),
-    url(r'^v3/docs/filtering/$', 'API.v3.views.docs_filtering', name='filter_docs'),
-    url(r'^v3/docs/ordering/$', 'API.v3.views.docs_ordering', name='ordering_docs'),
-    url(r'^v3/docs/about/$', 'API.v3.views.docs_about', name='about_docs'),
+    url(r'^v2/docs/resources/$', old_views.docs_resources, name='resource_docsv2'),
+    url(r'^v2/docs/filtering/$', old_views.docs_filtering, name='filter_docsv2'),
+    url(r'^v2/docs/ordering/$', old_views.docs_ordering, name='ordering_docsv2'),
+    url(r'^v2/docs/about/$', old_views.docs_about, name='about_docsv2'),
+    url(r'^v3/docs/', views.docs_index, name='docs'),
+    url(r'^v3/docs/getting-started/$', views.docs_start, name='start_docs'),
+    url(r'^v3/docs/resources/$', views.docs_resources, name='resource_docs'),
+    url(r'^v3/docs/filtering/$', views.docs_filtering, name='filter_docs'),
+    url(r'^v3/docs/ordering/$', views.docs_ordering, name='ordering_docs'),
+    url(r'^v3/docs/about/$', views.docs_about, name='about_docs'),
     url(r'^v3/indicator-data/$', 'API.v3.views.indicator_data_response'),
     url(r'^v3/activity-filter-options/$', 'API.v3.views.activity_filter_options'),
     url(r'^v3/indicator-filter-options/$', 'API.v3.views.indicator_filter_options'),
