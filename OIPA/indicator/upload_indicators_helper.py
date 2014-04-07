b219be5d50cce33257094e7aea9dcba9c9dd22e3
Berjan Bruens <berjan@bruens.it>
2014-04-07 12:08:54 +0200

    -add refactoring code -improved search algorithm countries en cities in database

--- OIPA/indicator/upload_indicators_helper.py
+++ OIPA/indicator/upload_indicators_helper.py
@@ -1,4 +1,6 @@
+import uuid
 from geodata.models import Country, City
+from indicator.models import CsvUploadLog, IndicatorData
 
 
 def get_countries():
@@ -15,11 +17,20 @@ def find_country(country_name, countries):
         @return: country from database or False if it could not map a country
         """
         #getting countries from database
-        for country in countries:
-            country_name_db = country.name
-            if country_name.decode('utf8') in country_name_db or country_name.decode('utf8')[:8] in country_name_db or country_name.decode('utf8')[-8:] in country_name_db:
-                return country
-        return False
+        try:
+            if country_name:
+                for country in countries:
+                    country_name_db = country.name.lower()
+                    country_name = country_name.lower()
+                    if country_name.decode('utf8') in country_name_db or country_name.decode('utf8')[:8] in country_name_db or country_name.decode('utf8')[-8:] in country_name_db:
+                        return country
+            else:
+                return None
+        except:
+            return None
+
+        return None
+
 
 def find_city(city_name, cities):
         """
@@ -29,13 +40,110 @@ def find_city(city_name, cities):
         @return: country from database or False if it could not map a country
         """
         #getting countries from database
-        for city in cities:
-            city_name = city_name.decode('utf8')
-            city_name_db = city.name
-            ascii_name_db = city.ascii_name
-            alt_name_db = city.alt_name
-            name_par_db = city.namepar
+        try:
+            if city_name:
+                for city in cities:
+                    city_name = city_name.lower()
 
-            if city_name in city_name_db or city_name[:8] in city_name_db or city_name[-8:] in city_name_db:
-                return city
+                    city_name = city_name.decode('utf8')
+                    city_name_db = city.name.lower()
+
+                    ascii_name_db = city.ascii_name
+                    alt_name_db = city.alt_name
+                    name_par_db = city.namepar
+
+                    if city_name in city_name_db or city_name[:8] in city_name_db or city_name[-8:] in city_name_db:
+                        return city
+            else:
+                return None
+        except:
+            return None
+
+        return None
+
+def get_value(value_csv):
+    value = None
+    if value_csv:
+        try:
+            value = float(value_csv)
+        except ValueError:
+            value = float(value_csv.replace('.', ''))
+    else:
+        value = None
+    return value
+
+
+def save_log(
+        file,
+        uploaded_by_user,
+        cities_not_found,
+        countries_not_found,
+        total_cities_found,
+        total_countries_found,
+        total_countries_not_found,
+        total_cities_not_found,
+        total_items_saved):
+
+    log = CsvUploadLog()
+    log.upload = file
+    log.uploaded_by = uploaded_by_user
+    log.slug = uuid.uuid4()
+    log.cities_not_found = unicode(', '.join(cities_not_found), errors='ignore')
+    log.countries_not_found = unicode(', '.join(countries_not_found), errors='ignore')
+    log.total_cities_found = total_cities_found.__len__()
+    log.total_countries_found = total_countries_found.__len__()
+    log.total_countries_not_found = total_countries_not_found.__len__()
+    log.total_cities_not_found = total_cities_not_found.__len__()
+    log.total_items_saved = total_items_saved
+    log.save()
+    return log
+
+def save_city_data(city_from_db, country_from_db, selection_type_csv, indicator_from_db, year_csv, value_csv):
+    try:
+        if city_from_db:
+            #if the indicator data a selection type contains than we need to store that correctly
+            if selection_type_csv:
+                indicator_data_from_db = IndicatorData.objects.get_or_create(year=year_csv, indicator=indicator_from_db, selection_type=selection_type_csv, city=city_from_db)[0]
+            else:
+                indicator_data_from_db = IndicatorData.objects.get_or_create(year=year_csv, indicator=indicator_from_db, city=city_from_db)[0]
+
+            #some city files also contain the country, so when this happens we also store it
+            if country_from_db:
+                indicator_data_from_db.country = country_from_db
+
+            indicator_data_from_db.city = city_from_db
+            #todo get region from db
+
+            indicator_data_from_db.value = get_value(value_csv=value_csv)
+            #todo add year range to model IndicatorData
+            #if year_range_csv:
+            #    indicator_data_from_db.year_range = year_range_csv
+            indicator_data_from_db.save()
+            return True
+        else:
+            return False
+    except:
+        #todo we need to know what is going wrong. We need to find the errors and give it back to the user.
+        return False
+
+def save_country_data(country_from_db, city_csv, selection_type_csv, year_csv, indicator_from_db, value_csv):
+    try:
+        if country_from_db and not city_csv:
+            if selection_type_csv:
+                indicator_data_from_db = IndicatorData.objects.get_or_create(year=year_csv, indicator=indicator_from_db, selection_type=selection_type_csv, country=country_from_db)[0]
+            else:
+                indicator_data_from_db = IndicatorData.objects.get_or_create(year=year_csv, indicator=indicator_from_db, country=country_from_db)[0]
+            #todo get region from db
+            indicator_data_from_db.value = get_value(value_csv=value_csv)
+
+
+            #todo add year range to model IndicatorData
+            #if year_range_csv:
+            #    indicator_data_from_db.year_range = year_range_csv
+            indicator_data_from_db.save()
+            return True
+        else:
+            return False
+    except:
+        #todo we need to know what is going wrong. We need to find the errors and give it back to the user.
         return False
