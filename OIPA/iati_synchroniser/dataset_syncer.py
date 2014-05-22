6e0f76e7cb90d4f22890edf0d4ba044fea0b43f7
Vincent van 't Westende <vincent@zimmermanzimmerman.nl>
2014-05-22 12:37:28 +0200

    improvements in logging

--- OIPA/iati_synchroniser/dataset_syncer.py
+++ OIPA/iati_synchroniser/dataset_syncer.py
@@ -4,9 +4,7 @@ import httplib
 import urllib2
 import datetime
 from iati.models import OrganisationIdentifier
-import logging
-
-logger = logging.getLogger(__name__)
+from iati_synchroniser.exception_handler import exception_handler
 
 class DatasetSyncer():
 
@@ -20,7 +18,7 @@ class DatasetSyncer():
             cur_url = "http://www.iatiregistry.org/api/search/dataset?extras_filetype=organisation&all_fields=1&limit=200&offset="
 
         for i in range(0, 10000, 200):
-            cur_url = cur_url.replace(" ", "%20")
+            cur_url = cur_url.strip().replace(" ", "%20")
             cur_url_with_offset = cur_url + str(i)
             self.synchronize_with_iati_api_by_page(cur_url_with_offset, type)
 
@@ -61,18 +59,14 @@ class DatasetSyncer():
                                     if "title" in data_dict["organization"]:
                                         publisher_name = data_dict["organization"]["title"]
                             except Exception as e:
-                                logger.info( "Unexpected error in synchronize_with_iati_api_by_page organisation match:")
-                                logger.info(e.message)
+                                exception_handler(e, "synchronize_with_iati_api_by_page", "Unexpected error in synchronize_with_iati_api_by_page organisation match:")
 
 
                         #   If download url is not already in OIPA
                         if not models.IatiXmlSource.objects.filter(source_url=source_url).exists():
 
-                            logger.info("starting on: " + source_url + ", cur url = " + cur_url)
-
-
                             #   If publisher_iati_id is given
-                            if not (publisher_iati_id is None) and not (publisher_iati_id == ""):
+                            if publisher_iati_id and (publisher_iati_id != ""):
 
                                 current_publisher = self.update_publisher(publisher_iati_id, publisher_abbreviation, publisher_name)
                             else:
@@ -84,70 +78,57 @@ class DatasetSyncer():
                             self.add_iati_xml_source_to_db(source_url, source_title, source_name, current_publisher, cur_type)
 
                         else:
-
-                            logger.info("Updated publisher and last found in registry on: " + source_url)
+                            exception_handler(None, "Updated publisher and last found in registry on: ", source_url)
 
                             cursource = models.IatiXmlSource.objects.get(source_url=source_url)
                             cursource.last_found_in_registry = datetime.datetime.now()
                             current_publisher = cursource.publisher
                             #check if publisher meta is already known, if not, add it and check if the known publisher already existed and add it to the source
-                            if ((current_publisher.org_abbreviate == "Unknown" and publisher_abbreviation and publisher_abbreviation != "Unknown") or (current_publisher.org_name == "Unknown" and publisher_name and publisher_name != "Unknown")):
+                            if (cursource.publisher.org_id != publisher_iati_id):
                                 new_current_publisher = self.update_publisher(publisher_iati_id, publisher_abbreviation, publisher_name)
                                 cursource.publisher = new_current_publisher
                             cursource.save(process=False, added_manually=False)
 
                     except Exception as e:
-                        logger.info("Unexpected error in synchronize_with_iati_api_by_page:")
-                        if e.args:
-                            logger.info(e.args[0])
-                            logger.info(e.args[1])
-                        if e.message:
-                            logger.info(e.message)
+                        exception_handler(e, "synchronize_with_iati_api_by_page", "Unexpected error")
 
 
 
         except urllib2.HTTPError, e:
-            logger.info('HTTP error: ' + str(e.code) + " " + cur_url)
+            exception_handler(e, "HTTP error", cur_url)
             if try_number < 6:
                 self.synchronize_with_iati_api_by_page(cur_url, cur_type,try_number + 1)
             else:
                 return None
         except urllib2.URLError, e:
-            logger.info('URL error: ' + str(e.reason))
+            exception_handler(e, "URL error", cur_url)
             if try_number < 6:
                 self.synchronize_with_iati_api_by_page(cur_url, cur_type,try_number + 1)
         except httplib.HTTPException, e:
-            logger.info('HTTP exception')
+            exception_handler(e, "HTTP Exception", cur_url)
             if try_number < 6:
                 self.synchronize_with_iati_api_by_page(cur_url, cur_type,try_number + 1)
 
 
     def update_publisher(self, publisher_iati_id, publisher_abbreviation, publisher_name):
 
-        # get the abbreviation from organisation_identifier table
-        if(OrganisationIdentifier.objects.filter(code=publisher_iati_id).exists()):
-            current_publisher_meta = OrganisationIdentifier.objects.get(code=publisher_iati_id)
-            publisher_abbreviation = current_publisher_meta.abbreviation
+        try:
+            #   if already in the database, get the publisher_id, else add the publisher
+            if (models.Publisher.objects.filter(org_id=publisher_iati_id).exists()):
+                current_publisher = models.Publisher.objects.get(org_id=publisher_iati_id)
+            else:
+                # get the abbreviation from organisation_identifier table
+                if(OrganisationIdentifier.objects.filter(code=publisher_iati_id).exists()):
+                    current_publisher_meta = OrganisationIdentifier.objects.get(code=publisher_iati_id)
+                    publisher_abbreviation = current_publisher_meta.abbreviation
 
-        #   if already in the database, get the publisher_id, else add the publisher
-        if (models.Publisher.objects.filter(org_abbreviate=publisher_iati_id).exists()):
-            current_publisher = models.Publisher.objects.get(org_abbreviate=publisher_iati_id)
+                current_publisher = self.add_publisher_to_db(publisher_iati_id, publisher_abbreviation, publisher_name)
 
-            # for legacy: update publisher if needed
-            if (current_publisher.org_id != publisher_iati_id):
-                current_publisher.org_id = publisher_iati_id
-            if (current_publisher.org_abbreviate != publisher_abbreviation):
-                current_publisher.org_abbreviate = publisher_abbreviation
-            if not current_publisher.org_name and publisher_name:
-                current_publisher.org_name = publisher_name
-            current_publisher.save()
+            return current_publisher
+            return current_publisher
 
-        elif (models.Publisher.objects.filter(org_id=publisher_iati_id).exists()):
-            current_publisher = models.Publisher.objects.get(org_id=publisher_iati_id)
-        else:
-            current_publisher = self.add_publisher_to_db(publisher_iati_id,publisher_abbreviation, publisher_name)
-
-        return current_publisher
+        except Exception as e:
+            exception_handler(e, publisher_iati_id, "dataset_syncer.update_publisher")
 
     def add_publisher_to_db(self, org_id, org_abbreviate_value, org_name_value):
         new_publisher = models.Publisher(org_id=org_id, org_abbreviate=org_abbreviate_value, org_name=org_name_value, default_interval='MONTHLY')
