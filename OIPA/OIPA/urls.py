f93357dfa5e4ac902030c1a02e6a91b8e925a0d8
Vincent van 't Westende <vincent@zimmermanzimmerman.nl>
2014-09-30 13:27:59 +0200

    test django rq urls #3

--- OIPA/OIPA/urls.py
+++ OIPA/OIPA/urls.py
@@ -2,14 +2,13 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
-from django_rq import urls as django_rq_urls
 from api.v3.urls import api_v3_docs
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    (r'^admin/queue/', include(django_rq_urls)),
+    (r'^admin/queue/', include('django_rq.urls')),
     url(r'^admin/task_queue/', include('task_queue.urls')),
     # url(r'^admin/geoadmin/', 'geodata.views.geoadmin'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
