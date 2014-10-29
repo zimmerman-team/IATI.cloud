eff94de80d6e56660bd7a85ca065ee99fc5a620e
Murrx <altenarobin@gmail.com>
2014-10-29 16:39:45 +0100

    changes based on feedback by @ngaranko

--- OIPA/OIPA/test_settings.py
+++ OIPA/OIPA/test_settings.py
@@ -3,17 +3,15 @@ from settings import *
 SECRET_KEY = 'j!bxt0h-=d)1@2r8du!+e4m9x-y*5od7+zq&=tfjwq(ecuov!*'
 
 DATABASES = {
-    'default': {
+    'mysql': {
         'ENGINE': 'django.contrib.gis.db.backends.mysql',
         'NAME': 'oipa_test', # <- change with database name
         'USER': 'root', # <- change with db user
 	'HOST': '127.0.0.1',
         'OPTIONS': { 'init_command': 'SET storage_engine=INNODB;' }
+    },
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+        'NAME': ':memory:',
     }
 }
-
-#DATABASES = {'default': {
-#        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
-#        'NAME': ':memory:',
-#    }
-#}
