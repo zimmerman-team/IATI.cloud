0a5e01ef43da8b425732e778d1c83f94c4be0cd5
Murrx <altenarobin@gmail.com>
2014-11-17 14:08:43 +0100

    added test settings to run tests on sqlite ram db

---
+++ OIPA/OIPA/test_settings.py
@@ -0,0 +1,10 @@
+from settings import *
+
+SECRET_KEY = 'j!bxt0h-=d)1@2r8du!+e4m9x-y*5od7+zq&=tfjwq(ecuov!*'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+        'NAME': ':memory:',
+    }
+}
