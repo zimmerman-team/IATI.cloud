b3786e3458cd8300480bdf9796caf990412ad791
Murrx <altenarobin@gmail.com>
2014-11-18 11:13:13 +0100

    added basic endpoint tests for the v3 api

--- OIPA/iati/factory/iati_factory.py
+++ OIPA/iati/factory/iati_factory.py
@@ -1,4 +1,5 @@
 import iati
+import geodata
 import factory
 
 
@@ -26,3 +27,51 @@ class ActivityFactory(factory.django.DjangoModelFactory):
     iati_identifier = 'IATI-0001'
     title_set = factory.RelatedFactory(TitleFactory, 'activity_id',)
     description_set = factory.RelatedFactory(DescriptionFactory, 'activity_id')
+
+
+class RegionFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = geodata.models.Region
+        django_get_or_create = ('code',)
+
+    code = 1
+    name = 'World'
+
+
+class CountryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = geodata.models.Country
+        django_get_or_create = ('code',)
+
+    code = 'AD'
+    name = 'andorra'
+    iso3 = 'and'
+
+
+class CityFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = geodata.models.City
+        django_get_or_create = ('id',)
+
+    id = 1
+    name = 'Colonia del Sacramento'
+    geoname_id = 3443013
+
+
+class SectorFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = iati.models.Sector
+        django_get_or_create = ('code',)
+
+    code = 200
+    name = 'advice'
+    description = ''
+
+
+class OrganisationFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = iati.models.Organisation
+        django_get_or_create = ('code',)
+
+    code = 'GB-COH-03580586'
+    name = 'PWC'
