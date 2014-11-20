24b1eccb99e28e032fe65fdd09ef4b8a80c49725
Murrx <altenarobin@gmail.com>
2014-11-20 16:51:56 +0100

    added basic repressentation of related_fields to organisation_serializer

--- OIPA/api/organisation/serializers.py
+++ OIPA/api/organisation/serializers.py
@@ -5,9 +5,33 @@ import iati
 class OrganisationDetailSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='organisation-detail')
 
+    activity_reporting_organisation = serializers.RelatedField(many=True)
+    activity_set = serializers.RelatedField(many=True)
+    activityparticipatingorganisation_set = serializers.RelatedField(many=True)
+    transaction_providing_organisation = serializers.RelatedField(many=True)
+    transaction_receiving_organisation = serializers.RelatedField(many=True)
+
     class Meta:
         model = iati.models.Organisation
-        fields = ()
+        fields = (
+            'url',
+            'code',
+            'abbreviation',
+            'type',
+            'reported_by_organisation',
+            'name',
+            'original_ref',
+
+            # Reverse linked data
+            'activity_reporting_organisation',
+            'activity_set',
+            'activityparticipatingorganisation_set',
+            'transaction_providing_organisation',
+            'transaction_receiving_organisation',
+        )
+
+    def transform_url(self, obj, value):
+        pass
 
 
 class OrganisationListSerializer(OrganisationDetailSerializer):
