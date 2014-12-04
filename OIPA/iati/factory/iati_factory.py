7be6fed18fd77eeb64469d6f46c9b5fe716a25f3
Murrx <altenarobin@gmail.com>
2014-12-04 13:54:12 +0100

    refactored iati_factory

--- OIPA/iati/factory/iati_factory.py
+++ OIPA/iati/factory/iati_factory.py
@@ -3,78 +3,66 @@ import geodata
 import factory
 
 
-class BudgetFactory(factory.django.DjangoModelFactory):
+class NoDatabaseFactory(factory.django.DjangoModelFactory):
+    @classmethod
+    def _setup_next_sequence(cls):
+        return 0
+
+
+class BudgetFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.Budget
+
     type_id = 1
     period_start = '2011-01-01 00:00:00'
     period_end = '2011-12-30 00:00:00'
     value = 100
     value_date = '2013-06-28'
 
-    @classmethod
-    def _setup_next_sequence(cls):
-        return 0
 
-
-class CurrencyFactory(factory.django.DjangoModelFactory):
+class CurrencyFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.Currency
+
     code = 'USD'
     name = 'us dolar'
 
-    @classmethod
-    def _setup_next_sequence(cls):
-        return 0
 
-
-class CollaborationTypeFactory(factory.django.DjangoModelFactory):
+class CollaborationTypeFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.CollaborationType
+
     code = 1
     name = 'Bilateral'
 
-    @classmethod
-    def _setup_next_sequence(cls):
-        return 0
 
-
-class ActivityStatusFactory(factory.django.DjangoModelFactory):
+class ActivityStatusFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.ActivityStatus
+
     code = 1
     name = 'Pipeline/identification'
 
-    @classmethod
-    def _setup_next_sequence(cls):
-        return 0
 
-
-class FlowTypeFactory(factory.django.DjangoModelFactory):
+class FlowTypeFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.FlowType
+
     code = 1
     name = 'test-flowtype'
     description = 'test-flowtype-description'
 
-    @classmethod
-    def _setup_next_sequence(cls):
-        return 0
 
-
-class AidTypeCategoryFactory(factory.django.DjangoModelFactory):
+class AidTypeCategoryFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.AidTypeCategory
+
     code = 1
     name = 'test-category'
     description = 'test-category-description'
 
-    @classmethod
-    def _setup_next_sequence(cls):
-        return 0
 
-
-class AidTypeFactory(factory.django.DjangoModelFactory):
+class AidTypeFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.AidType
 
@@ -83,34 +71,22 @@ class AidTypeFactory(factory.django.DjangoModelFactory):
     description = 'test'
     category = factory.SubFactory(AidTypeCategoryFactory)
 
-    @classmethod
-    def _setup_next_sequence(cls):
-        return 0
 
-
-class TitleFactory(factory.django.DjangoModelFactory):
+class TitleFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.Title
 
     title = 'title factory'
 
-    @classmethod
-    def _setup_next_sequence(cls):
-        return 0
 
-
-class DescriptionFactory(factory.django.DjangoModelFactory):
+class DescriptionFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.Description
 
     description = 'description factory'
 
-    @classmethod
-    def _setup_next_sequence(cls):
-        return 0
 
-
-class ActivityFactory(factory.django.DjangoModelFactory):
+class ActivityFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.Activity
 
@@ -118,63 +94,53 @@ class ActivityFactory(factory.django.DjangoModelFactory):
     total_budget = 50
     iati_identifier = 'IATI-0001'
 
-    @classmethod
-    def _setup_next_sequence(cls):
-        return 0
 
-
-class RegionFactory(factory.django.DjangoModelFactory):
+class RegionFactory(NoDatabaseFactory):
     class Meta:
         model = geodata.models.Region
-        django_get_or_create = ('code',)
 
     code = 1
     name = 'World'
 
 
-class CountryFactory(factory.django.DjangoModelFactory):
+class CountryFactory(NoDatabaseFactory):
     class Meta:
         model = geodata.models.Country
-        django_get_or_create = ('code',)
 
     code = 'AD'
     name = 'andorra'
     iso3 = 'and'
 
 
-class CityFactory(factory.django.DjangoModelFactory):
+class CityFactory(NoDatabaseFactory):
     class Meta:
         model = geodata.models.City
-        django_get_or_create = ('id',)
 
     id = 1
     name = 'Colonia del Sacramento'
     geoname_id = 3443013
 
 
-class SectorFactory(factory.django.DjangoModelFactory):
+class SectorFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.Sector
-        django_get_or_create = ('code',)
 
     code = 200
     name = 'advice'
     description = ''
 
 
-class OrganisationFactory(factory.django.DjangoModelFactory):
+class OrganisationFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.Organisation
-        django_get_or_create = ('code',)
 
     code = 'GB-COH-03580586'
     name = 'PWC'
 
 
-class ActivitySectorFactory(factory.django.DjangoModelFactory):
+class ActivitySectorFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.ActivitySector
-        django_get_or_create = ('id',)
 
     id = 1
     sector = factory.SubFactory(SectorFactory)
@@ -182,20 +148,18 @@ class ActivitySectorFactory(factory.django.DjangoModelFactory):
     percentage = 100
 
 
-class ParticipatingOrganisationFactory(factory.django.DjangoModelFactory):
+class ParticipatingOrganisationFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.ActivityParticipatingOrganisation
-        django_get_or_create = ('id',)
 
     id = 1
     activity = factory.SubFactory(ActivityFactory)
     organisation = factory.SubFactory(OrganisationFactory)
 
 
-class RecipientCountryFactory(factory.django.DjangoModelFactory):
+class RecipientCountryFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.ActivityRecipientCountry
-        django_get_or_create = ('id',)
 
     id = 1
     activity = factory.SubFactory(ActivityFactory)
@@ -203,47 +167,34 @@ class RecipientCountryFactory(factory.django.DjangoModelFactory):
     percentage = 50
 
 
-class PolicyMarkerFactory(factory.django.DjangoModelFactory):
+class PolicyMarkerFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.PolicyMarker
+
     code = 1
     name = 'Gender Equality'
 
-    @classmethod
-    def _setup_next_sequence(cls):
-        return 0
 
-
-class VocabularyFactory(factory.django.DjangoModelFactory):
+class VocabularyFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.Vocabulary
+
     code = 1
     name = 'OECD DAC CRS'
 
-    @classmethod
-    def _setup_next_sequence(cls):
-        return 0
 
-
-class PolicySignificanceFactory(factory.django.DjangoModelFactory):
+class PolicySignificanceFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.PolicySignificance
+
     code = 0
     name = 'not targeted'
     description = 'test description'
 
-    @classmethod
-    def _setup_next_sequence(cls):
-        return 0
 
-
-class ActivityPolicyMarkerFactory(factory.django.DjangoModelFactory):
+class ActivityPolicyMarkerFactory(NoDatabaseFactory):
     class Meta:
         model = iati.models.ActivityPolicyMarker
-
-    @classmethod
-    def _setup_next_sequence(cls):
-        return 0
 
     policy_marker = PolicyMarkerFactory.build()
     alt_policy_marker = 'alt_policy_marker'
