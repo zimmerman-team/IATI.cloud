ad513db5f3904deaada463f206e4b9e225b5fd46
vpl <vpl@vpl.me>
2014-12-05 15:13:31 +0100

    Changed import

--- OIPA/api/generics.py
+++ OIPA/api/generics.py
@@ -1,12 +1,9 @@
 from rest_framework import generics
-from rest_framework import pagination as drf_pagination
-from rest_framework import serializers as drf_serializers
+from rest_framework.pagination import DefaultObjectSerializer
+from rest_framework.serializers import ListSerializer
 
 
 def get_dynamic_pagination_serializer(self, page):
-    # get serializer never gets called because ListAPIView calls paginator.
-    # paginator class def get_serializer_class but not get_serializer
-    # overwrite pagination serializer since it's the method that does get called
     class SerializerClass(self.pagination_serializer_class):
         class Meta:
             object_serializer_class = self.get_serializer_class()
@@ -21,10 +18,10 @@ def get_dynamic_pagination_serializer(self, page):
             try:
                 object_serializer = self.Meta.object_serializer_class
             except AttributeError:
-                object_serializer = drf_pagination.DefaultObjectSerializer
+                object_serializer = DefaultObjectSerializer
 
-            self.fields[results_field] = drf_serializers.ListSerializer(
-                child=object_serializer(args, kwargs, fields=fields_arg),
+            self.fields[results_field] = ListSerializer(
+                child=object_serializer(fields=fields_arg),
                 source='object_list'
             )
 
@@ -34,8 +31,7 @@ def get_dynamic_pagination_serializer(self, page):
     request_fields = self.request.QUERY_PARAMS.get('fields', None)
 
     if request_fields is not None:
-        self.fields = []
-        self.fields.extend(request_fields.split(','))
+        self.fields = request_fields.split(',')
 
     return pagination_serializer_class(
         instance=page, context=context, fields=self.fields)
@@ -46,8 +42,10 @@ def get_dynamic_serializer(self, instance=None, data=None, many=False, partial=F
     context = self.get_serializer_context()
     fields = serializer_class.Meta.fields
     request_fields = self.request.QUERY_PARAMS.get('fields', None)
+
     if request_fields is not None:
         fields = request_fields.split(',')
+
     return serializer_class(
         instance, data=data, many=many, partial=partial, context=context, fields=fields)
 
