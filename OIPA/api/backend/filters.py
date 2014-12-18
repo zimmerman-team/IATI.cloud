e2d1d60838a0f03780b7b5b25b5d87169fe39cbc
Murrx <altenarobin@gmail.com>
2014-12-18 17:19:28 +0100

    fix error when no q_fields are used

--- OIPA/api/backend/filters.py
+++ OIPA/api/backend/filters.py
@@ -4,8 +4,9 @@ from rest_framework import filters
 class SearchFilter(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         query = request.QUERY_PARAMS.get('q', None)
-        query_fields = request.QUERY_PARAMS.get('q_fields', None)
-        query_fields = query_fields.split(',')
         if query:
+            query_fields = request.QUERY_PARAMS.get('q_fields')
+            if query_fields:
+                query_fields = query_fields.split(',')
             return queryset.search(query, query_fields)
         return queryset.all()
