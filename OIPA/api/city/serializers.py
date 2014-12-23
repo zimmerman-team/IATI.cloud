ff2319391dc3d0fd6f70906dfd7e2e9b05054ee7
Murrx <altenarobin@gmail.com>
2014-12-23 15:54:57 +0100

    added proper serialization to location field

--- OIPA/api/city/serializers.py
+++ OIPA/api/city/serializers.py
@@ -5,16 +5,24 @@ from api.country.serializers import CountrySerializer
 from indicator.models import Indicator
 
 
+class GeometryField(serializers.Field):
+    def to_representation(self, obj):
+        return {
+            'type': 'Point',
+            'coordinates': [obj.x, obj.y]
+        }
+
+
 class IndicatorDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = Indicator
         fields = ('id',)
-        unique = True
 
 
 class CitySerializer(DynamicFieldsModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='city-detail')
     country = CountrySerializer(fields=('url', 'code', 'name'))
+    location = GeometryField()
 
     class Meta:
         model = geodata.models.City
