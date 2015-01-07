b0a5e6142e43468452a49148f766ca23bc5185e2
Murrx <altenarobin@gmail.com>
2015-01-07 16:51:36 +0100

    fixed organisation-detail url by encoding organisation.code

--- OIPA/api/organisation/serializers.py
+++ OIPA/api/organisation/serializers.py
@@ -1,10 +1,23 @@
+import django.utils.http
 import iati
 from rest_framework import serializers
 from api.generics.serializers import DynamicFieldsModelSerializer
 
 
 class OrganisationSerializer(DynamicFieldsModelSerializer):
-    url = serializers.HyperlinkedIdentityField(view_name='organisation-detail')
+    class EncodedHyperlinkedIdentityField(
+            serializers.HyperlinkedIdentityField):
+        def get_url(self, obj, view_name, request, format):
+            if obj.pk is None:
+                return None
+            lookup_value = getattr(obj, self.lookup_field)
+            quoted_lookup_value = django.utils.http.urlquote(lookup_value)
+
+            kwargs = {self.lookup_url_kwarg: quoted_lookup_value}
+            return self.reverse(
+                view_name, kwargs=kwargs, request=request, format=format)
+
+    url = EncodedHyperlinkedIdentityField(view_name='organisation-detail')
 
     class Meta:
         model = iati.models.Organisation
