f6e402eb6e7186e6103a413d041701cc18ffbd95
Murrx <altenarobin@gmail.com>
2015-01-07 17:59:43 +0100

    fixed organisation detail view by decoding the url to its orriginal form

--- OIPA/api/organisation/views.py
+++ OIPA/api/organisation/views.py
@@ -1,3 +1,5 @@
+from django.utils.http import urlunquote
+from django.shortcuts import get_object_or_404
 import iati
 from api.organisation import serializers
 from api.generics.views import DynamicListAPIView
@@ -13,3 +15,13 @@ class OrganisationList(DynamicListAPIView):
 class OrganisationDetail(DynamicRetrieveAPIView):
     queryset = iati.models.Organisation.objects.all()
     serializer_class = serializers.OrganisationSerializer
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+        lookup_url = self.kwargs[lookup_url_kwarg]
+
+        decoded_lookup_url = urlunquote(lookup_url)
+        filter_kwargs = {self.lookup_field: decoded_lookup_url}
+
+        return get_object_or_404(queryset, **filter_kwargs)
