aff99d1ee6167dde3324138650e2119df432d54b
Murrx <altenarobin@gmail.com>
2015-01-24 15:00:58 +0100

    pep8 formatting on indicator.models

--- OIPA/indicator/models.py
+++ OIPA/indicator/models.py
@@ -5,22 +5,27 @@ from geodata.models import Country, City, Region
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
+
 class IndicatorTopic(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     source_note = models.TextField(null=True)
 
+
 class LendingType(models.Model):
     id = models.CharField(max_length=10, primary_key=True)
     name = models.CharField(max_length=255)
+
 
 class IncomeLevel(models.Model):
     id = models.CharField(max_length=10, primary_key=True)
     name = models.CharField(max_length=255)
 
+
 class IndicatorSource(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.TextField()
+
 
 class Indicator(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
@@ -53,7 +58,12 @@ class IndicatorData(models.Model):
 class IndicatorDataValue(models.Model):
     indicator_data = models.ForeignKey(IndicatorData, related_name='values')
     year = models.IntegerField(max_length=5, db_index=True)
-    value = models.DecimalField(null=True, blank=True, db_index=True, max_digits=17, decimal_places=4)
+    value = models.DecimalField(
+        null=True,
+        blank=True,
+        db_index=True,
+        max_digits=17,
+        decimal_places=4)
 
 
 def file_upload_to(instance, filename):
@@ -64,20 +74,29 @@ def file_upload_to(instance, filename):
         # when file has no extension
         name = filename
         ext = None
-    name = slugify(name).replace('-','_')
-    return os.path.join(path, '%s%s%s' % (name, ext and '.' or '',
-        ext or ''))
+    name = slugify(name).replace('-', '_')
+    return os.path.join(path, '%s%s%s' % (name, ext and '.' or '', ext or ''))
+
 
 class CsvUploadLog(models.Model):
     upload_date = models.DateTimeField(_('Upload date'), auto_now_add=True)
-    upload = models.FileField(_('File'), upload_to=file_upload_to,
-        blank=True, null=True, max_length=255)
-    title = models.CharField(_('title'), max_length=100, null=True, blank=True)
+    upload = models.FileField(
+        _('File'),
+        upload_to=file_upload_to,
+        blank=True,
+        null=True,
+        max_length=255)
+    title = models.CharField(
+        _('title'), max_length=100, null=True, blank=True)
     slug = models.SlugField(_('slug'), max_length=100, unique=True)
     link = models.URLField(_('link'), max_length=500, null=True, blank=True)
-    description = models.CharField(_('description'), blank=True, max_length=200)
-    uploaded_by = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True,
-        verbose_name=_(u'uploaded by'), related_name='uploaded_files')
+    description = models.CharField(
+        _('description'), blank=True, max_length=200)
+    uploaded_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        blank=True, null=True,
+        verbose_name=_(u'uploaded by'),
+        related_name='uploaded_files')
     cities_not_found = models.TextField(null=True, blank=True)
     countries_not_found = models.TextField(null=True, blank=True)
     total_countries_found = models.IntegerField(null=True, blank=True)
