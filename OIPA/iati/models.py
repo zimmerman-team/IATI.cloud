00a12cb37d399fec47f20bb7f837991f59bac347
daanzap <daniel@zimmermanzimmerman.nl>
2015-10-13 15:22:47 +0200

    fixed typo in models

--- OIPA/iati/models.py
+++ OIPA/iati/models.py
@@ -103,7 +103,7 @@ class Activity(models.Model):
     objects = ActivityQuerySet.as_manager()
 
     def __unicode__(self):
-        return self.ido
+        return self.id
 
     class Meta:
         verbose_name_plural = "activities"
