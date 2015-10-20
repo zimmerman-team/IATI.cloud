9d4f3a3e2d37d07e0112d1b6d85d16568292ee96
Vincent van 't Westende <vincentvantwestende@hotmail.com>
2015-10-20 14:14:36 +0200

    remove deprecated travis folder

--- .travis/build_pysqlite2.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-wget https://pypi.python.org/packages/source/p/pysqlite/pysqlite-2.6.3.tar.gz
-tar xzf pysqlite-2.6.3.tar.gz
-cd pysqlite-2.6.3
-sed -i'' '6,6 s/^/#/' setup.cfg
-python setup.py install
