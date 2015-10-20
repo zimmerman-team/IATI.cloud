35c46d9c817f17ce29b951dea5b138f6223fb8ac
Vincent van 't Westende <vincentvantwestende@hotmail.com>
2015-10-20 16:25:09 +0200

    spatialite test

--- OIPA/OIPA/test_settings.py
+++ OIPA/OIPA/test_settings.py
@@ -1,9 +1,16 @@
 from settings import *
 
+SECRET_KEY = 'j!bxt0h-=d)1@2r8du!+e4m9x-y*5od7+zq&=tfjwq(ecuov!*'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.spatialite',
         'NAME': ':memory:',
-    }
+    },
+    'psql': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '',
+    },
 }
-
