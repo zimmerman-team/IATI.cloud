aaf92d21854f9c1f2d1749b1cc6a4432219840d4
Bryan Haakman <bryanhaakman@gmail.com>
2015-11-25 13:42:52 +0100

    disable activity aggregations for now until further discussion

--- OIPA/api/activity/aggregation.py
+++ OIPA/api/activity/aggregation.py
@@ -64,18 +64,18 @@ class AggregationsPaginationSerializer(NoCountPaginationSerializer):
     
     def get_paginated_response(self, data):
 
-        if (self.aggregations.data):
-            return Response(OrderedDict([
-                ('count', self.page.paginator.count),
-                ('next', self.get_next_link()),
-                ('previous', self.get_previous_link()),
-                ('aggregations', self.aggregations.data),
-                ('results', data),
-            ]))
-        else:
-            return Response(OrderedDict([
-                ('count', self.page.paginator.count),
-                ('next', self.get_next_link()),
-                ('previous', self.get_previous_link()),
-                ('results', data),
-            ]))
+        # if (self.aggregations.data):
+        #     return Response(OrderedDict([
+        #         ('count', self.page.paginator.count),
+        #         ('next', self.get_next_link()),
+        #         ('previous', self.get_previous_link()),
+        #         ('aggregations', self.aggregations.data),
+        #         ('results', data),
+        #     ]))
+        # else:
+        return Response(OrderedDict([
+            ('count', self.page.paginator.count),
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('results', data),
+        ]))
