8cec22bca540a47d736968fe5d084e396e5d3b61
Vincent van 't Westende <vincentvantwestende@hotmail.com>
2016-01-12 13:57:43 +0100

    update travis test settings

--- OIPA/OIPA/travis_test_settings.py
+++ OIPA/OIPA/travis_test_settings.py
@@ -4,13 +4,13 @@ SECRET_KEY = 'j!bxt0h-=d)1@2r8du!+e4m9x-y*5od7+zq&=tfjwq(ecuov!*'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
-        'NAME': ':memory:',
-    },
-    'psql': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': '',
     },
+    'sqlite': {
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+        'NAME': ':memory:',
+    },    
 }
