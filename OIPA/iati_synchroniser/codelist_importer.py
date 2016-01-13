e65469268cd47af033e02e7082d8c528afc7ca59
Vincent van 't Westende <vincentvantwestende@hotmail.com>
2016-01-13 12:08:02 +0100

    prevent new codelist items to break codelist updating

--- OIPA/iati_synchroniser/codelist_importer.py
+++ OIPA/iati_synchroniser/codelist_importer.py
@@ -148,6 +148,7 @@ class CodeListImporter():
                 model = apps.get_model(app_label='geodata', model_name=model_name)
             except LookupError:
                 print ''.join(['Model not found: ', model_name])
+                return False
 
         if not item:
             item = model()
