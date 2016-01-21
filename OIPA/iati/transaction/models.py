6e54be5bf341f074a181459636208006b435b16e
Bryan Haakman <bryanhaakman@gmail.com>
2016-01-21 14:41:24 +0100

    Transaction model changes w.r.t changing which foreign keys are required

--- OIPA/iati/transaction/models.py
+++ OIPA/iati/transaction/models.py
@@ -21,30 +21,32 @@ from iati.models import Narrative
 
 class Transaction(models.Model):
     activity = models.ForeignKey(Activity)
-    aid_type = models.ForeignKey(AidType, null=True, blank=True, default=None)
+
+    ref = models.CharField(max_length=255, null=True, blank=True, default="")
+
+    transaction_type = models.ForeignKey(
+        TransactionType)
+    transaction_date = models.DateField()
+
+    value = models.DecimalField(max_digits=15, decimal_places=2)
+    value_string = models.CharField(max_length=50)
+    currency = models.ForeignKey(Currency)
+    value_date = models.DateField()
+
 
     disbursement_channel = models.ForeignKey(
         DisbursementChannel,
         null=True,
         blank=True,
         default=None)
-    finance_type = models.ForeignKey(FinanceType, null=True, blank=True, default=None)
-    flow_type = models.ForeignKey(FlowType, null=True, blank=True, default=None)
 
-    tied_status = models.ForeignKey(TiedStatus, null=True, blank=True, default=None)
-    transaction_date = models.DateField(null=True, blank=True, default=None)
-    transaction_type = models.ForeignKey(
-        TransactionType,
-        null=True,
-        blank=True,
-        default=None)
-    value_date = models.DateField(null=True, blank=True, default=None)
-    value = models.DecimalField(max_digits=15, decimal_places=2)
-    value_string = models.CharField(max_length=50)
-    currency = models.ForeignKey(Currency, null=True, blank=True, default=None)
-    ref = models.CharField(max_length=255, null=True, blank=True, default="")
-    recipient_region = models.ForeignKey(Region, null=True, blank=True)
+    recipient_region = models.ForeignKey(Region, null=True, blank=True, default=None)
     recipient_country = models.ForeignKey(Country, null=True, blank=True, default=None)
+
+    flow_type = models.ForeignKey(FlowType, null=True, blank=True, default=None)
+    finance_type = models.ForeignKey(FinanceType, null=True, blank=True, default=None)
+    aid_type = models.ForeignKey(AidType, null=True, blank=True, default=None)
+    tied_status = models.ForeignKey(TiedStatus, null=True, blank=True, default=None)
 
     objects = TransactionQuerySet.as_manager()
 
