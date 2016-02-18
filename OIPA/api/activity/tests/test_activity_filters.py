700c228af57c266b780cb2553986f08c3be1a39a
Bryan Haakman <bryanhaakman@gmail.com>
2016-02-18 17:04:40 +0100

    fix imports

--- OIPA/api/activity/tests/test_activity_filters.py
+++ OIPA/api/activity/tests/test_activity_filters.py
@@ -2,6 +2,6 @@ from unittest import skip
 from django.test import TestCase
 
 from iati.models import ActivitySearch, Activity
-from iati.factory.utils import _create_test_activity, create_test_narrative
+from iati.factory.utils import _create_test_activity, _create_test_narrative
 
 # TODO: desc - 2016-02-18
