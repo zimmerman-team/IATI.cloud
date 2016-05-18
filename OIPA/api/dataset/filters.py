0bf5aaeaf83dc00903f7049e4d64fb7210e3d271
Vincent van 't Westende <vincentvantwestende@hotmail.com>
2016-05-18 18:20:00 +0200

    publisher filter on datasets endpoint

--- OIPA/api/dataset/filters.py
+++ OIPA/api/dataset/filters.py
@@ -1,12 +1,16 @@
-import django_filters
+from api.generics.filters import CommaSeparatedCharFilter
 from iati_synchroniser.models import IatiXmlSource
+from django_filters import FilterSet
 
 
-class DatasetFilter(django_filters.FilterSet):
+class DatasetFilter(FilterSet):
     """
     Filter countries list
     """
-    # region_code = django_filters.NumberFilter(name='region__code')
+
+    publisher = CommaSeparatedCharFilter(
+        lookup_type='in',
+        name='publisher__org_id')
 
     class Meta:
         model = IatiXmlSource
