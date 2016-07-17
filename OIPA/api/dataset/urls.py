459050ef78acd24cb0d58c36622dd931bceac0d6
Vincent van 't Westende <vincentvantwestende@hotmail.com>
2016-07-17 12:46:01 +0200

    cache dataset aggregations

--- OIPA/api/dataset/urls.py
+++ OIPA/api/dataset/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls import url
 from api.dataset import views
-
+from django.views.decorators.cache import cache_page
+from OIPA.production_settings import API_CACHE_SECONDS
 
 urlpatterns = [
     url(r'^$',
         views.DatasetList.as_view(), name='dataset-list'),
     url(r'^aggregations/',
-        views.DatasetAggregations.as_view(),
+        cache_page(API_CACHE_SECONDS)(views.DatasetAggregations.as_view()),
         name='dataset-aggregations'),
-    url(
-        r'^(?P<pk>[^@$&+,/:;=?]+)/$',
+    url(r'^(?P<pk>[^@$&+,/:;=?]+)/$',
         views.DatasetDetail.as_view(),
         name='dataset-detail'),
     url(r'^(?P<pk>[^@$&+,/:;=?]+)/notes/',
