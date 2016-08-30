3b594a6d5cbf546890634e00229efc0378e9f7cf
Vincent van 't Westende <vincentvantwestende@hotmail.com>
2016-08-30 17:15:32 +0200

    only run tests on spatialite

--- OIPA/OIPA/travis_test_settings.py
+++ OIPA/OIPA/travis_test_settings.py
@@ -4,9 +4,7 @@ SECRET_KEY = 'j!bxt0h-=d)1@2r8du!+e4m9x-y*5od7+zq&=tfjwq(ecuov!*'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '',
-    }, 
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+        'NAME': ':memory:',
+    },    
 }
