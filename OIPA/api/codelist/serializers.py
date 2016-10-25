eb526ccfa1b5cc7240b1e9623b6e72b2a9051919
Vincent van 't Westende <vincentvantwestende@hotmail.com>
2016-10-25 08:34:56 +0200

    codelist API

--- OIPA/api/codelist/serializers.py
+++ OIPA/api/codelist/serializers.py
@@ -1,29 +1,34 @@
 from rest_framework import serializers
-
-import iati
-from api.generics.serializers import DynamicFieldsSerializer
+from iati.models import Narrative, DocumentCategory
+from iati_synchroniser.models import Codelist
+from api.generics.serializers import DynamicFieldsSerializer, DynamicFieldsModelSerializer
+from django.core.urlresolvers import reverse
 
 
 class VocabularySerializer(serializers.Serializer):
     code = serializers.CharField()
     name = serializers.CharField()
 
+
 class CodelistSerializer(DynamicFieldsSerializer):
     code = serializers.CharField()
     name = serializers.CharField(required=False)
 
+
 class CodelistCategorySerializer(CodelistSerializer):
     category = CodelistSerializer()
 
+
 class CodelistVocabularySerializer(CodelistSerializer):
     vocabulary = VocabularySerializer()
+
 
 class NarrativeSerializer(serializers.ModelSerializer):
     text = serializers.CharField(source="content")
     language = CodelistSerializer(required=False)
 
     class Meta:
-        model = iati.models.Narrative
+        model = Narrative
         fields = (
             'text',
             'language',
@@ -32,11 +37,37 @@ class NarrativeSerializer(serializers.ModelSerializer):
     # def validate(self, data):
     #     print('called validate...')
 
+
 class NarrativeContainerSerializer(serializers.Serializer):
     narratives = NarrativeSerializer(many=True)
 
+
 class DocumentCategorySerializer(serializers.ModelSerializer):
     class Meta:
-        model = iati.models.DocumentCategory
+        model = DocumentCategory
         fields = ('code', 'name')
 
+
+class CodelistItemSerializer(DynamicFieldsModelSerializer):
+    code = serializers.CharField()
+    name = serializers.CharField(required=False)
+    vocabulary = VocabularySerializer(required=False)
+    category = CodelistSerializer(required=False)
+
+    class Meta:
+        model = None
+        fields = ('code', 'name', 'vocabulary', 'category')
+
+
+class CodelistMetaSerializer(DynamicFieldsModelSerializer):
+    name = serializers.CharField()
+    items = serializers.SerializerMethodField()
+
+    def get_items(self, obj):
+        request = self.context.get('request')
+        url = request.build_absolute_uri(reverse('codelists:codelist-items-list', kwargs={'codelist': obj.name}))
+        return url
+
+    class Meta:
+        model = Codelist
+        fields = ('name', 'items')
