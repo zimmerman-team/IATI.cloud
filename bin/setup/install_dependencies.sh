20d0b5f4fadfd5f2f717898f22b4738d33c05bd2
Vincent van 't Westende <vincentvantwestende@hotmail.com>
2016-11-01 13:53:25 +0100

    update install dependencies to pg 9.5 / Ubuntu 16.04

--- bin/setup/install_dependencies.sh
+++ bin/setup/install_dependencies.sh
@@ -13,13 +13,14 @@ sudo dpkg-reconfigure locales
 # postgresql
 sudo apt-get install -y sqlite3 # for tests
 sudo apt-get install -y libsqlite3-dev # for tests
-sudo apt-get install -y postgresql-9.3
+sudo apt-get install -y postgresql-9.5
 sudo apt-get install -y postgresql-client
-sudo apt-get install -y postgresql-server-dev-9.3
+sudo apt-get install -y postgresql-server-dev-9.5
 sudo apt-get install -y postgis
-sudo apt-get install -y postgresql-9.3-postgis-2.1
-sudo apt-get install -y postgresql-9.3-postgis-2.1-scripts
-
+sudo apt-get install -y postgresql-9.5-postgis-2.3
+sudo apt-get install -y postgresql-9.5-postgis-2.3-scripts
+sudo apt-get install python-psycopg2
+sudo apt-get install libpq-dev
 # GEOS
 sudo apt-get install -y binutils libproj-dev gdal-bin libgeos-3.4.2 libgeos-dev
 
