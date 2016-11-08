f2caec61e193bf408980b4db2a6b15354bf78f07
HatimMoha <timland.py@gmail.com>
2016-11-08 01:59:10 +0100

    Add supervisor launch in Vagrant User profile bashrc

--- bin/setup/update_bashrc.sh
+++ bin/setup/update_bashrc.sh
@@ -6,6 +6,6 @@ else
     echo "Updating ~/.bashrc..."
     sudo -u vagrant echo "cd /vagrant/OIPA" >> /home/vagrant/.bashrc
     sudo -u vagrant echo "echo 'Starting supervisor...'"
-    sudo -u vagrant echo "python ./manage.py supervisor --daemonize --logfile static/supervisor.log"
+    sudo -u vagrant echo "python ./manage.py supervisor --daemonize" >> /home/vagrant/.bashrc
     sudo -u vagrant echo "echo 'Started.'"
 fi
