621a781dc1bc0d30b88b3c5ebd56f581efbab762
Vincent van 't Westende <vincentvantwestende@hotmail.com>
2016-11-08 15:00:27 +0100

    remove a left-over printline

--- OIPA/OIPA/urls.py
+++ OIPA/OIPA/urls.py
@@ -30,8 +30,6 @@ urlpatterns = [
 handler404 = views.error404
 handler500 = views.error500
 
-print settings.DEBUG
-
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
