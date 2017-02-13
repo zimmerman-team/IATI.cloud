ca1df5b28f8f719f50da894b095c7d749834f135
Bryan Haakman <bryanhaakman@gmail.com>
2017-02-13 15:10:46 +0200

    settings fix

--- OIPA/iati/activity_search_indexes.py
+++ OIPA/iati/activity_search_indexes.py
@@ -12,7 +12,7 @@ from django.conf import settings
 
 # TODO: prefetches - 2016-01-07
 def reindex_activity(activity):
-    if settings.get('FTS_ENABLED') == False:
+    if getattr(settings, 'FTS_ENABLED') == False:
         return
 
     try:
