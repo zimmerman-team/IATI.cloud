f30c84a16ae7daee331c8f3abb92a11edbe73745
Eimantas <eimantas.stonys@gmail.com>
2018-05-29 17:12:37 +0200

    Code style fixes for api.generics app
    OIPA-612 / #589

--- OIPA/api/generics/tests/test_no_count_serializer.py
+++ OIPA/api/generics/tests/test_no_count_serializer.py
@@ -1,8 +1,3 @@
-# from api.generics.serializers import NoCountPaginationSerializer
-from django.test import RequestFactory
-
 # TODO: disabled for now, enable again and write tests - 2016-02-18
-
-
 class NoCountSerializerTestCase:
     pass
--- OIPA/api/generics/tests/test_together_filterset.py
+++ OIPA/api/generics/tests/test_together_filterset.py
@@ -1,15 +1,11 @@
-
-from unittest import skip
 from django.test import TestCase
 
-from iati.models import ActivitySearch, Activity
-# from iati.factory.utils import _create_test_activity, create_test_narrative
+from iati.models import Activity
 
 from api.generics.filters import TogetherFilterSet
 from django.test.client import RequestFactory
 
 from django_filters import DateFilter
-from api.generics.filters import CommaSeparatedCharFilter
 
 
 class TestFilter(TogetherFilterSet):
@@ -45,10 +41,13 @@ class TogetherFilterSetTestCase(TestCase):
 
     def test_filter_is_applied_together(self):
         """
-        Test when specifying together_exclusive, the filters are applied together
+        Test when specifying together_exclusive, the filters are applied
+        together
         """
 
-        request = RequestFactory().get('/api/activities/?budget_period_start=2015-04-01&budget_period_end=2016-03-31')
+        request = RequestFactory().get(
+            '/api/activities/?budget_period_start=2015-04-01&budget_period_end=2016-03-31'  # NOQA: E501
+        )
         query_params = request.GET
 
         queryset = Activity.objects.all()
@@ -61,7 +60,9 @@ class TogetherFilterSetTestCase(TestCase):
         self.assertEqual(len(query.__dict__['alias_map']), 2)
 
     def test_without_together_exclusive(self):
-        request = RequestFactory().get('/api/activities/?budget_period_start=2015-04-01&budget_period_end=2016-03-31')
+        request = RequestFactory().get(
+            '/api/activities/?budget_period_start=2015-04-01&budget_period_end=2016-03-31'  # NOQA: E501
+        )
         query_params = request.GET
 
         queryset = Activity.objects.all()
