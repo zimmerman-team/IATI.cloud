68babd4b6e6124f02bc36b76564832b7715ebe5b
Eimantas Stonys <zimmerman-es@eimantass-mbp.home>
2018-06-07 14:21:19 +0200

    Don't hardcode external library 'djorm_pgfulltext'..
    ..inside the project. OIPA-612 / #589

--- OIPA/iati/djorm_pgfulltext/gis.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-from django.contrib.gis.db.models import GeoManager
-from .models import SearchManagerMixIn
-
-
-class GisSearchManager(SearchManagerMixIn, GeoManager):
-    pass
