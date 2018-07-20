3c0efce1efd695d78623716db3c6c7d3395e7221
Eimantas Stonys <zimmerman-es@eimantass-mbp.home>
2018-07-20 11:26:50 +0200

    Implement logic for parsing result indocator's..
    ,,doxument-link element. Also updated some docstrings. See: OIPA-87 / #650

--- OIPA/iati/parser/IATI_2_03.py
+++ OIPA/iati/parser/IATI_2_03.py
@@ -3000,7 +3000,7 @@ class Parse(IatiParser):
 
     # TODO: test
     def iati_activities__iati_activity__result__document_link(self, element):
-        '''New (optional) element in 2.03
+        '''New (optional) <document-link> element for <result> element in 2.03
         '''
         url = element.attrib.get('url')
 
@@ -3049,21 +3049,34 @@ class Parse(IatiParser):
         return element
 
     # TODO: test
-    def iati_activities__iati_activity__result__document_link__title__narrative(
+    def iati_activities__iati_activity__result__document_link__title(
             self, element):
-        """attributes:
+        '''New (optional) <document-link> element for <result> element in 2.03
+        '''
 
-        tag:narrative"""
+        document_link = self.get_model('DocumentLink')
+
+        document_link_title = models.DocumentLinkTitle()
+        document_link_title.document_link = document_link
+
+        self.register_model('DocumentLinkTitle', document_link_title)
+
+        return element
+
+    # TODO: test
+    def iati_activities__iati_activity__result__document_link__title__narrative(  # NOQA: E501
+            self, element):
+        '''New (optional) <document-link> element for <result> element in 2.03
+        '''
         document_link_title = self.get_model('DocumentLinkTitle')
         self.add_narrative(element, document_link_title)
         return element
 
     # TODO: test
-    def iati_activities__iati_activity__result__document_link__category(self, element):
-        """attributes:
-        code:A01
-
-        tag:category"""
+    def iati_activities__iati_activity__result__document_link__category(
+            self, element):
+        '''New (optional) <document-link> element for <result> element in 2.03
+        '''
         code = element.attrib.get('code')
         category = self.get_or_none(
             codelist_models.DocumentCategory, code=code)
@@ -3094,11 +3107,10 @@ class Parse(IatiParser):
         return element
 
     # TODO: test
-    def iati_activities__iati_activity__result__document_link__language(self, element):
-        """attributes:
-        code:en
-
-        tag:language"""
+    def iati_activities__iati_activity__result__document_link__language(
+            self, element):
+        '''New (optional) <document-link> element for <result> element in 2.03
+        '''
         code = element.attrib.get('code')
         language = self.get_or_none(codelist_models.Language, code=code)
 
@@ -3236,6 +3248,151 @@ class Parse(IatiParser):
         description = self.get_model('ResultIndicatorDescription')
         self.add_narrative(element, description)
 
+        return element
+
+    # TODO: test
+    def iati_activities__iati_activity__result__indicator__document_link(
+            self, element):
+        '''New (optional) <document-link> element for <indicator> element
+           inside <result> element in 2.03
+        '''
+        url = element.attrib.get('url')
+
+        file_format_code = element.attrib.get('format')
+        file_format = self.get_or_none(
+            codelist_models.FileFormat, code=file_format_code)
+
+        if not url:
+            raise RequiredFieldError(
+                "document-link",
+                "url",
+                "required attribute missing")
+
+        if not file_format_code:
+            raise RequiredFieldError(
+                "document-link",
+                "format",
+                "required attribute missing")
+
+        if not file_format:
+            raise FieldValidationError(
+                "document-link",
+                "format",
+                "not found on the accompanying code list",
+                None,
+                None,
+                file_format_code)
+
+        activity = self.get_model('Activity')
+        result_indicator = self.get_model('ResultIndicator')
+
+        # It is impossible to assign related object (ForeignKey) before it's
+        # saved, so:
+        # XXX: not sure how efficient this is.
+        if result_indicator.pk is None:
+            result_indicator.save()
+
+        document_link = models.DocumentLink()
+        document_link.activity = activity
+        document_link.url = url
+        document_link.file_format = file_format
+        document_link.result_indicator = result_indicator
+
+        self.register_model('DocumentLink', document_link)
+
+        return element
+
+    # TODO: test
+    def iati_activities__iati_activity__result__indicator__document_link__title(  # NOQA: E501
+            self, element):
+        '''New (optional) <document-link> element for <indicator> element
+           inside <result> element in 2.03
+        '''
+
+        document_link = self.get_model('DocumentLink')
+
+        document_link_title = models.DocumentLinkTitle()
+        document_link_title.document_link = document_link
+
+        self.register_model('DocumentLinkTitle', document_link_title)
+
+        return element
+
+    # TODO: test
+    def iati_activities__iati_activity__result__indicator__document_link__title__narrative(  # NOQA: E501
+            self, element):
+        '''New (optional) <document-link> element for <indicator> element
+           inside <result> element in 2.03
+        '''
+        document_link_title = self.get_model('DocumentLinkTitle')
+        self.add_narrative(element, document_link_title)
+        return element
+
+    # TODO: test
+    def iati_activities__iati_activity__result__indicator__document_link__category(self, element):  # NOQA: E501
+        '''New (optional) <document-link> element for <indicator> element
+           inside <result> element in 2.03
+        '''
+        code = element.attrib.get('code')
+        category = self.get_or_none(
+            codelist_models.DocumentCategory, code=code)
+
+        if not code:
+            raise RequiredFieldError(
+                "document-link/category",
+                "code",
+                "required attribute missing")
+
+        if not category:
+            raise FieldValidationError(
+                "document-link/category",
+                "code",
+                "not found on the accompanying code list",
+                None,
+                None,
+                code)
+
+        document_link = self.get_model('DocumentLink')
+
+        document_link_category = models.DocumentLinkCategory()
+
+        document_link_category.document_link = document_link
+        document_link_category.category = category
+
+        self.register_model('DocumentLinkCategory', document_link_category)
+        return element
+
+    # TODO: test
+    def iati_activities__iati_activity__result__indicator__document_link__language(self, element):  # NOQA: E501
+        '''New (optional) <document-link> element for <indicator> element
+           inside <result> element in 2.03
+        '''
+        code = element.attrib.get('code')
+        language = self.get_or_none(codelist_models.Language, code=code)
+
+        if not code:
+            raise RequiredFieldError(
+                "document-link/language",
+                "code",
+                "required attribute missing")
+
+        if not language:
+            raise FieldValidationError(
+                "document-link/language",
+                "code",
+                "not found on the accompanying code list",
+                None,
+                None,
+                code)
+
+        document_link = self.get_model('DocumentLink')
+
+        document_link_language = models.DocumentLinkLanguage()
+
+        document_link_language.document_link = document_link
+        document_link_language.language = language
+
+        self.register_model('DocumentLinkLanguage', document_link_language)
         return element
 
     # tag:baseline"""
