b329a2cd00baa5c8652dafa6485ba4a1a0070448
Eimantas Stonys <eimantas.stonys@gmail.com>
2018-09-14 14:41:08 +0200

    Remove single 'aid_type' relationship from..
    ..Transaction model. See: #762

--- OIPA/api/transaction/filters.py
+++ OIPA/api/transaction/filters.py
@@ -496,7 +496,6 @@ class TransactionFilter(FilterSet):
         model = Transaction
         fields = [
             'id',
-            'aid_type',
             'transaction_type',
             'value',
             'min_value',
