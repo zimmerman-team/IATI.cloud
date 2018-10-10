193764a2f4e7d0be166682bf91d56bd35df3cacf
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2018-10-10 10:06:49 +0200

    Added the endpoint sector-budgets to counting a total budget of the current sector and the total budget has related to the current budget

--- OIPA/api/unesco/urls.py
+++ OIPA/api/unesco/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import url
 
-from api.unesco.views import TransactionBalanceAggregation
+from api.unesco.views import (
+    TransactionBalanceAggregation,
+    SectorBudgetAggregations
+)
 
 app_name = 'api'
 urlpatterns = [
     url(r'^transaction-balance-aggregations',
         TransactionBalanceAggregation.as_view(),
         name='transaction-balance-aggregations'),
+    url(r'^sector-budgets',
+        SectorBudgetAggregations.as_view({'get': 'list'}),
+        name='sector-budgets'),
 ]
