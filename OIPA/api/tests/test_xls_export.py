8b6ae8070dda5caae32b52506a3b4ebcf7d30b12
fus <martynas@zimmermanzimmerman.nl>
2018-11-29 17:06:57 +0200

    unit test with factory done!!!

--- OIPA/api/tests/test_xls_export.py
+++ OIPA/api/tests/test_xls_export.py
@@ -3,10 +3,12 @@ import datetime
 import urllib.request
 from django.test import TestCase, RequestFactory
 from rest_framework.reverse import reverse
+
+from iati.models import Activity
 from iati.parser.parse_manager import ParseManager
 from lxml.builder import E
 
-from iati.factory.iati_factory import (ResultFactory)
+from iati.factory.iati_factory import (ResultFactory, ActivityFactory)
 from iati_synchroniser.factory import synchroniser_factory
 
 from django.test import Client
@@ -16,37 +18,13 @@ import xlrd
 
 class TestFilter(TestCase):
 
-    # def setUp(self):
-    #
-    #     # file_name, headers = c.urlretrieve(api_url)
-    #
-    #
-    #     # 'Main' XML file for instantiating parser:
-    #     xml_file_attrs = {
-    #         "generated-datetime": datetime.datetime.now().isoformat(),
-    #         "version": '2.03',
-    #     }
-    #
-    #     self.iati_203_XML_file = E("iati-activities", **xml_file_attrs)
-    #
-    #     dummy_source = synchroniser_factory.DatasetFactory.create()
-    #
-    #     self.parser_203 = ParseManager(
-    #         dataset=dummy_source,
-    #         root=self.iati_203_XML_file,
-    #     ).get_parser()
-    #
-    #     factory = ResultFactory.create()
-    #     api_url = reverse('activities:activity-list')
-    #     # factory = RequestFactory()
-    #     resp = factory.get(api_url)
-    #     lol = 'lol'
+    def setUp(self):
+        ActivityFactory()
 
     # basically downloads some data in xls format
     # with export fields applied and checks if the fields exist
     def test_xls_export_fields(self):
-
-        link = reverse('activities:activity-list') + '?export_fields=' \
+        link = reverse('activities:activity-list') + '1/?export_fields=' \
                                                      '%7B%22title.narratives.0.text%22%3A%22Project+' \
                                                      'title%22%2C%22title.id%22%3A%22Project+id%22%2C%22iati_' \
                                                      'identifier%22%3A%22IATI+Identifier%22%7D&format=xls'
