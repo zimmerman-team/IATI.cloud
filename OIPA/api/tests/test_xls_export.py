c26f9c69981a898d786b93589a7707c7306e7587
fus <martynas@zimmermanzimmerman.nl>
2018-11-29 18:16:25 +0200

    super anti hardcoded

--- OIPA/api/tests/test_xls_export.py
+++ OIPA/api/tests/test_xls_export.py
@@ -8,28 +8,30 @@ from iati.factory.iati_factory import ActivityFactory
 class TestFilter(TestCase):
 
     def setUp(self):
-        ActivityFactory()
+        # This is basically how the export fields look,
+        # that are used in the
+        # below url
+        self.export_fields = {
+            "title.narratives.0.text": "Project title",
+            "title.id": "Project id",
+            "iati_identifier": "IATI Identifier"
+        }
+
+        self.export_field_params = '/?export_fields=' \
+            '%7B%22title.narratives.0.text%22%3A%22Project+' \
+            'title%22%2C%22title.id%22%3A%22' \
+            'Project+id%22%2C%22iati_' \
+            'identifier%22%3A%22IATI+Identifier%22%7D&format=xls'
+
+        self.activity = ActivityFactory()
 
     # basically downloads some data in xls format
     # with export fields applied and checks if the fields exist
     def test_xls_export_fields(self):
         link = reverse('activities:activity-list')
-        link = link + '1/?export_fields=' \
-                      '%7B%22title.narratives.0.text%22%3A%22Project+' \
-                      'title%22%2C%22title.id%22%3A%22' \
-                      'Project+id%22%2C%22iati_' \
-                      'identifier%22%3A%22IATI+Identifier%22%7D&format=xls'
+        link = link + str(self.activity.id) + self.export_fields
         c = Client()
         response = c.get(link)
-
-        # This is basically how the export fields look,
-        # that are used in the
-        # below url
-        export_fields = {
-            "title.narratives.0.text": "Project title",
-            "title.id": "Project id",
-            "iati_identifier": "IATI Identifier"
-        }
 
         workbook = xlrd.open_workbook(file_contents=response.content)
 
@@ -38,7 +40,8 @@ class TestFilter(TestCase):
 
         for idx, cell_obj in enumerate(row):
             column_name = cell_obj.value
-            column_name_exists = column_name in export_fields.values()
+            column_name_exists = column_name in self.export_fields.values()
             self.assertTrue(column_name_exists)
-            export_fields = {key: val for key, val in export_fields.items()
-                             if val != column_name}
+            self.export_fields = {key: val for key, val in
+                                  self.export_fields.items()
+                                  if val != column_name}
