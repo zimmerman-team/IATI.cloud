39aa0284d0747ad204936922231d95a8ca045008
luminhan <lumin@zimmermanzimmerman.nl>
2018-12-11 09:40:11 +0100

    - isort and flake8 fixed

--- OIPA/api/branch/views.py
+++ OIPA/api/branch/views.py
@@ -1,6 +1,7 @@
-from rest_framework.response import Response
-from rest_framework.generics import ListAPIView
 import subprocess
+
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
 
 
 class GitBranch(ListAPIView):
