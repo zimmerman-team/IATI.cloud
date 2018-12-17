c426eacbbe925756ae7d39211c891ce910394d6d
Eimantas Stonys <eimantas.stonys@gmail.com>
2018-12-17 15:43:44 +0100

    Fix Grapelli-related error to avoid e-mail..
    ..flooding

--- OIPA/OIPA/views.py
+++ OIPA/OIPA/views.py
@@ -6,7 +6,7 @@ from django.template import loader
 ##
 # Handle 404 Errors
 # @param request WSGIRequest list with all HTTP Request
-def error404(request):
+def error404(request, exception):
 
     # Generate Content for this view
     template = loader.get_template('404.html')
