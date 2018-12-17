e256ecc03b251291438de4430e84cd3ffb742070
Eimantas Stonys <eimantas.stonys@gmail.com>
2018-12-17 11:44:53 +0100

    Use SMTP email settings in production, also..
    ..update README

--- OIPA/OIPA/production_settings.py
+++ OIPA/OIPA/production_settings.py
@@ -86,3 +86,10 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
     },
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
