6e274a19e4be6e7cd1ec5fa413692c1447b839ae
Eimantas Stonys <eimantas.stonys@gmail.com>
2018-12-18 15:32:41 +0100

    Fix wrong migration reference

--- OIPA/geodata/migrations/0004_remove_country_capital_city.py
+++ OIPA/geodata/migrations/0004_remove_country_capital_city.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('geodata', '0003_remove_region_added_manually'),
+        ('geodata', '0002_region_added_manually'),
     ]
 
     operations = [
