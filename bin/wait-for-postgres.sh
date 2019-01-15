b482a18a50ebdc6553b0d8aaa17d94a28de1b736
Eimantas Stonys <eimantas.stonys@gmail.com>
2019-01-15 10:45:25 +0100

    Comment-out some more unneeded shell scripts,..
    ..add TODO comments. WIP. See: #781

--- bin/wait-for-postgres.sh
+++ bin/wait-for-postgres.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
-# Waits for Postgres to be available and runs a command
-# wait-for-postgres.sh
+##!/bin/bash
+## Waits for Postgres to be available and runs a command
+## wait-for-postgres.sh
 
-set -e
+#set -e
 
-cmd="$@"
+#cmd="$@"
 
-until PGPASSWORD=${OIPA_DB_PASSWORD} psql -h "${OIPA_DB_HOST}" -U "${OIPA_DB_USER}" ${OIPA_DB_NAME} -c '\l' > /dev/null; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
+#until PGPASSWORD=${OIPA_DB_PASSWORD} psql -h "${OIPA_DB_HOST}" -U "${OIPA_DB_USER}" ${OIPA_DB_NAME} -c '\l' > /dev/null; do
+  #>&2 echo "Postgres is unavailable - sleeping"
+  #sleep 1
+#done
 
->&2 echo "Postgres is up - executing command"
-exec $cmd
+#>&2 echo "Postgres is up - executing command"
+#exec $cmd
