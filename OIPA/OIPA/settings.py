8a2f4ff8a56b8ad1f0254310e7cfb2b6b49cafe1
Eimantas Stonys <eimantas.stonys@gmail.com>
2019-02-20 13:27:04 +0100

    Triple redis queue defailt worker timeout time..
    ..because downloading task takes more than 1 hour

--- OIPA/OIPA/settings.py
+++ OIPA/OIPA/settings.py
@@ -232,7 +232,7 @@ RQ_REDIS_URL = env.get('OIPA_RQ_REDIS_URL', 'redis://localhost:6379/0')
 RQ_QUEUES = {
     'default': {
         'URL': RQ_REDIS_URL,
-        'DEFAULT_TIMEOUT': 3600,
+        'DEFAULT_TIMEOUT': 10800,  # 3 hours
     },
     'parser': {
         'URL': RQ_REDIS_URL,
