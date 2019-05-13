9a21ad9d9545d819e15d8f422907f5e407ebe0c3
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2019-05-13 16:39:00 +0200

    Changed to existing serializer

--- OIPA/api/organisation/serializers.py
+++ OIPA/api/organisation/serializers.py
@@ -141,21 +141,6 @@ class TotalBudgetBudgetLineSerializer(ModelSerializerNoValidation):
         return update_instance
 
 
-class TotalBudgetSerializer(ModelSerializerNoValidation):
-    currency = CodelistSerializer(required=False)
-
-    class Meta:
-        model = org_models.TotalBudget
-        fields = (
-            'status',
-            'period_start',
-            'period_end',
-            'value_date',
-            'currency',
-            'value',
-        )
-
-
 class OrganisationTotalBudgetSerializer(ModelSerializerNoValidation):
 
     organisation = serializers.CharField(write_only=True)
@@ -1062,7 +1047,10 @@ class OrganisationSerializer(DynamicFieldsModelSerializer):
 
     reporting_org = OrganisationReportingOrganisationSerializer(read_only=True)
 
-    total_budgets = TotalBudgetSerializer(many=True, read_only=True)
+    total_budgets = OrganisationTotalBudgetSerializer(
+        many=True,
+        read_only=True
+    )
 
     class Meta:
         model = org_models.Organisation
