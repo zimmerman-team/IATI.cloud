ed29038d8b2453c32fbd57581301228036760644
Lu Min Han <luminn@zimmermanzimmerman.nl>
2019-06-20 17:12:28 +0200

    enable `all` parameters to get all the serializer fields defined in Serializer class.

--- OIPA/api/generics/views.py
+++ OIPA/api/generics/views.py
@@ -58,7 +58,14 @@ class DynamicView(GenericAPIView):
 
         request_fields = self.request.query_params.get('fields')
 
-        if request_fields:
+        # if requested query fields is set to `all` we will return all
+        # serializer fields defined in serialier class. Here we assign
+        # `self.fields = ()` so that it will be assigned all serializer
+        # fields in `filter_queryset` method.
+        
+        if request_fields and request_fields == 'all':
+            self.fields = ()
+        elif request_fields:
             for request_field in request_fields.split(','):
                 if request_field not in list(self.fields):
                     # put selectable fields together with required fields
