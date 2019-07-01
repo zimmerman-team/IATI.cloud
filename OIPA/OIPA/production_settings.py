67934c48a54be7f6b2c5a5e867ed0e5508bc653d
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2019-07-01 18:28:13 +0200

    Updated memcached library & set default cached in production config

--- OIPA/OIPA/production_settings.py
+++ OIPA/OIPA/production_settings.py
@@ -83,7 +83,7 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
         'LOCATION': '127.0.0.1:11211',
-        'TIMEOUT': 60 * 60 * 24 * 30,
+        'TIMEOUT': 60 * 60,
     },
 }
 
