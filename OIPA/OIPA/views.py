c0988299a54de8c59c6927779098ab8825dc3712
Taufik Hidayat <t.hidayat.id@gmail.com>
2019-08-19 11:19:58 +0200

    Redirect home to api url

--- OIPA/OIPA/views.py
+++ OIPA/OIPA/views.py
@@ -1,6 +1,7 @@
 # Imports
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import redirect
 
 
 ##
@@ -32,3 +33,7 @@ def error500(request):
     # Return Template for this view + Data
     return HttpResponse(content=template.render(context),
                         content_type='text/html; charset=utf-8', status=500)
+
+
+def home(request):
+    return redirect('/api')
