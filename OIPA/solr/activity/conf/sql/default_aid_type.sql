1ff7faf9035df7c34de9dc149329a551a243cd08
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2019-09-09 16:24:53 +0200

    Added default aid type category

--- OIPA/solr/activity/conf/sql/default_aid_type.sql
+++ OIPA/solr/activity/conf/sql/default_aid_type.sql
@@ -1,6 +1,8 @@
                     SELECT
                            aid_type_id,
                            vocabulary_id,
+                           ica.code AS category_code,
+                           ica.name AS category_name,
                            CAST((
                                SELECT
                                       ROW_TO_JSON(default_aid_type_record) AS default_aid_type
@@ -11,5 +13,8 @@
                                 ) AS default_aid_type_record
                             ) AS VARCHAR)
                     FROM iati_activitydefaultaidtype, iati_codelists_aidtype
-                    WHERE iati_activitydefaultaidtype.aid_type_id = iati_codelists_aidtype.code
-                        /* AND activity_id=${activity.id} */
+                    LEFT OUTER JOIN iati_codelists_aidtypecategory ica on iati_codelists_aidtype.category_id = ica.code
+                    /*
+                    WHERE activity_id=${activity.id}
+                        AND iati_activitydefaultaidtype.aid_type_id = iati_codelists_aidtype.code
+                     */
