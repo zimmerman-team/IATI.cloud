172f1004886afc9f9497b0d5a558cc83db89c8ba
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2019-10-29 11:52:51 +0100

    Updated not used code

--- OIPA/solr/indexing.py
+++ OIPA/solr/indexing.py
@@ -3,13 +3,5 @@ from __future__ import print_function
 
 import pysolr
 
-from iati.models import Activity
-from solr.serializers import ActualSerializer
-
 # Create a client instance. The timeout and authentication options are not required.
 solr = pysolr.Solr('http://localhost:8983/solr/activity', always_commit=True)
-
-
-def run():
-    for activity in Activity.objects.all():
-        solr.add(ActualSerializer(activity).data)
