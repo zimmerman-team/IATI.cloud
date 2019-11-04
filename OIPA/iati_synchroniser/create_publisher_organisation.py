3293ae47da50488ad7484406b24e7f10d7f7606e
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2019-11-04 21:48:04 +0100

    Added publisher & organisation indexing

--- OIPA/iati_synchroniser/create_publisher_organisation.py
+++ OIPA/iati_synchroniser/create_publisher_organisation.py
@@ -1,8 +1,16 @@
+import logging
+
 from iati_codelists.models import Language, OrganisationType, Version
 from iati_organisation.models import (
     Organisation, OrganisationName, OrganisationNarrative,
     OrganisationReportingOrganisation
 )
+
+from solr.organisation.tasks import OrganisationTaskIndexing
+from solr.publisher.tasks import PublisherTaskIndexing
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 
 def create_publisher_organisation(publisher, publisher_organization_type):
@@ -50,4 +58,15 @@ def create_publisher_organisation(publisher, publisher_organization_type):
 
     publisher.organisation = org
     publisher.save()
+
+    try:
+        OrganisationTaskIndexing(instance=publisher.organisation).run()
+    except Exception as e:
+        logger.exception(e)
+
+    try:
+        PublisherTaskIndexing(instance=publisher).run()
+    except Exception as e:
+        logger.exception(e)
+
     return publisher
