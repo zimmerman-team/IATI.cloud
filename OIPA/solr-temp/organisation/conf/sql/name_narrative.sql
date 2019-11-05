a0e92df555fb199bbfeee29e1305b904da540466
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2019-11-05 10:56:27 +0100

    Removed temp code

--- OIPA/solr-temp/organisation/conf/sql/name_narrative.sql
+++
@@ -1,9 +0,0 @@
-                        SELECT
-                            iati_narrative.content AS content
-                        FROM iati_organisation_organisationnarrative, django_content_type
-                        /*
-                        WHERE iati_organisation_organisationnarrative.object_id=${organisation_name.id}
-                            AND iati_organisation_organisationnarrative.organisation_id=${organisation.id}
-                            AND django_content_type.model='organisationname'
-                            AND django_content_type.id=iati_organisation_organisationnarrative.content_type_id
-                         */
