345b4339492f60df738b0c077a863a7449f8a864
Taufik Hidayat <taufik@zimmermanzimmerman.nl>
2020-02-17 17:29:02 +0100

    Updated PEP8

--- OIPA/iati_synchroniser/tests/test_dataset_syncer.py
+++ OIPA/iati_synchroniser/tests/test_dataset_syncer.py
@@ -1,7 +1,6 @@
-import unittest
-
 import json
 import os
+import unittest
 
 from django.test import TestCase
 from mock import MagicMock
--- OIPA/iati_synchroniser/tests/test_m49_regions_importer.py
+++ OIPA/iati_synchroniser/tests/test_m49_regions_importer.py
@@ -1,6 +1,5 @@
-import unittest
-
 import os
+import unittest
 
 from django.test import TestCase
 
