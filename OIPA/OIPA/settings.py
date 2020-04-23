b4268471da84c93d5b85782884efc393dff5b3f2
helene <helene@zadmins-mbp.home>
2020-04-23 15:48:51 +0200

    settings for m49 codes import

--- OIPA/OIPA/settings.py
+++ OIPA/OIPA/settings.py
@@ -340,15 +340,15 @@ REST_FRAMEWORK_EXTENSIONS = {
 # For example, for M49 Regions import, add such code block it in the
 # local_settings.py:
 
-# import os
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-# DATA_PLUGINS = {
-#     'codelist': {
-#        'm49_region_file': '{base_dir}/plugins/data/{filename}'.format(
-#             base_dir=BASE_DIR, filename='regions.json')
-#     }
-# }
-DATA_PLUGINS = {}
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+DATA_PLUGINS = {
+    'codelist': {
+       'm49_region_file': '{base_dir}/plugins/data/{filename}'.format(
+            base_dir=BASE_DIR, filename='regions.json')
+    }
+}
+#DATA_PLUGINS = {}
 
 # A setting indicating whether to save XML datasets (files) to local machine or
 # not:
