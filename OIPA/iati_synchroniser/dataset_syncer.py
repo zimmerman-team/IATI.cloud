acfbf1f2f46af11e4d88df561839f430173e841b
Lu Min Han <luminn@zimmermanzimmerman.nl>
2020-08-04 23:01:31 +0200

    add timeout =30 sec to requests in dataset_syncer.py and download.py

--- OIPA/iati_synchroniser/dataset_syncer.py
+++ OIPA/iati_synchroniser/dataset_syncer.py
@@ -161,11 +161,13 @@ class DatasetSyncer(object):
                                  '10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}  # NOQA: E501
 
         try:
-            response = requests.get(source_url, headers=headers)
+            response = requests.get(source_url, headers=headers, timeout=30)
         except requests.exceptions.SSLError:
-            response = requests.get(source_url, verify=False, headers=headers)
+            response = requests.get(source_url, verify=False,
+                                    headers=headers, timeout=30)
         except (requests.exceptions.ConnectionError,
-                requests.exceptions.TooManyRedirects):
+                requests.exceptions.TooManyRedirects,
+                requests.exceptions.Timeout):
             pass
 
         if response and response.status_code == 200:
