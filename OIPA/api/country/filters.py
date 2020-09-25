bffd5458f4cf37fa579ea64321af0237298d994d
Loïc Lehmann <loic.lehmann@hotmail.com>
2020-09-25 11:32:30 +0200

    Fixed name call returning server error 500, the field was missing his filter in publishers/filters.py

--- OIPA/api/country/filters.py
+++ OIPA/api/country/filters.py
@@ -2,12 +2,19 @@ import django_filters
 
 from geodata.models import Country
 
+from api.generics.filters import CommaSeparatedCharFilter
 
 class CountryFilter(django_filters.FilterSet):
     """
     Filter countries list
     """
-    region_code = django_filters.NumberFilter(field_name='region__code')
+    region_code = django_filters.NumberFilter(
+        field_name='region__code'
+    )
+    name = CommaSeparatedCharFilter(
+        field_name='name',
+        lookup_expr='in'
+    )
 
     class Meta:
         model = Country
