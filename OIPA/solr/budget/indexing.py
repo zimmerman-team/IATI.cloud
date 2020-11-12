f4da45fb7205768003999d2927522939c1221f56
Loïc Lehmann <loic.lehmann@hotmail.com>
2020-11-12 15:44:59 +0100

    Added required fields for budget core

--- OIPA/solr/budget/indexing.py
+++ OIPA/solr/budget/indexing.py
@@ -9,6 +9,11 @@ def add_recipient_country(serializer, activity):
     if recipient_country_all:
         serializer.add_field('recipient_country_code', [])
         serializer.add_field('recipient_country_name', [])
+        serializer.add_field('recipient_country_percentage', [])
+
+        serializer.add_field('recipient_country_narrative', [])
+        serializer.add_field('recipient_country_narrative_lang', [])
+        serializer.add_field('recipient_country_narrative_text', [])
 
         for recipient_country in recipient_country_all:
             serializer.add_value_list(
@@ -18,6 +23,69 @@ def add_recipient_country(serializer, activity):
             serializer.add_value_list(
                 'recipient_country_name',
                 recipient_country.country.name
+            )
+            serializer.add_value_list(
+                'recipient_country_percentage',
+                recipient_country.percentage
+            )
+            serializer.related_narrative(
+                recipient_country,
+                'recipient_country_narrative',
+                'recipient_country_narrative_lang',
+                'recipient_country_narrative_text'
+            )
+
+
+def add_recipient_region(serializer, activity):
+    recipient_region_all = activity.activityrecipientregion_set.all()
+    if recipient_region_all:
+
+        serializer.add_field('recipient_region_code', [])
+        serializer.add_field('recipient_region_name', [])
+
+        serializer.add_field('recipient_region_vocabulary', [])
+        serializer.add_field('recipient_region_vocabulary_uri', [])
+
+        serializer.add_field('recipient_region_percentage', [])
+
+        serializer.add_field('recipient_region_narrative', [])
+        serializer.add_field('recipient_region_narrative_lang', [])
+        serializer.add_field('recipient_region_narrative_text', [])
+
+        for recipient_region in recipient_region_all:
+            serializer.add_value_list(
+                'recipient_region_code',
+                recipient_region.region.code
+            )
+            serializer.add_value_list(
+                'recipient_region_name',
+                recipient_region.region.name
+            )
+            if recipient_region.vocabulary.code:
+                serializer.add_value_list(
+                    'recipient_region_vocabulary',
+                    recipient_region.vocabulary.code
+                )
+            else:
+                serializer.indexing['recipient_region_vocabulary'].append(' ')
+            if recipient_region.vocabulary_uri:
+                serializer.add_value_list(
+                    'recipient_region_vocabulary_uri',
+                    recipient_region.vocabulary_uri
+                )
+            else:
+                serializer.indexing['recipient_region_vocabulary_uri'].append(
+                    ' ')
+            serializer.add_value_list(
+                'recipient_region_percentage',
+                recipient_region.percentage
+            )
+
+            serializer.related_narrative(
+                recipient_region,
+                'recipient_region_narrative',
+                'recipient_region_narrative_text',
+                'recipient_region_narrative_lang'
             )
 
 
@@ -55,18 +123,6 @@ class BudgetIndexing(BaseIndexing):
         self.add_field('budget_value_date_f', date_string(budget.value_date))
 
         self.add_field('budget_value', decimal_string(budget.value))
-        self.add_field(
-            'budget_value_usd',
-            decimal_string(budget.usd_value)
-        )
-        self.add_field(
-            'budget_imf_link',
-            budget.imf_url
-        )
-        self.add_field(
-            'budget_usd_conversion_rate',
-            decimal_string(budget.usd_exchange_rate)
-        )
 
         self.add_field(
             'humanitarian',
@@ -75,6 +131,7 @@ class BudgetIndexing(BaseIndexing):
 
         add_reporting_org(self, budget.activity)
         add_recipient_country(self, budget.activity)
+        add_recipient_region(self, budget.activity)
 
     def to_representation(self, budget):
         self.record = budget
