55dd577eeeec86d4c71cf2846e0ea850031827cd
Sylvan Ridderinkhof <sylvanridderinkhof@gmail.com>
2021-05-28 13:50:51 +0200

    chore: iati/parser url update

--- OIPA/iati/parser/IATI_2_02.py
+++ OIPA/iati/parser/IATI_2_02.py
@@ -1241,7 +1241,7 @@ class Parse(IatiParser):
 
     def iati_activities__iati_activity__location__point(self, element):
         """attributes:
-        srsName:http://www.opengis.net/def/crs/EPSG/0/4326
+        srsName:https://www.opengis.net/def/crs/EPSG/0/4326
 
         tag:point"""
         srs_name = element.attrib.get('srsName')
@@ -1250,7 +1250,7 @@ class Parse(IatiParser):
         # if not srs_name: raise RequiredFieldError("srsName",
         # "location_point: srsName is required")
         if not srs_name:
-            srs_name = "http://www.opengis.net/def/crs/EPSG/0/4326"
+            srs_name = "https://www.opengis.net/def/crs/EPSG/0/4326"
 
         location = self.get_model('Location')
         location.point_srs_name = srs_name
