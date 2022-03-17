8ff057ad57e41efd02d2f0e461c53da277f1b2ab
Sylvan Ridderinkhof <sylvanridderinkhof@gmail.com>
2022-03-17 15:58:13 +0100

    feat: introduce common start and end date functionality

--- OIPA/direct_indexing/custom_fields/activity_dates.py
+++ OIPA/direct_indexing/custom_fields/activity_dates.py
@@ -23,8 +23,19 @@ def activity_dates(data):
 
 def extract_activity_dates(date, data):
     # This approach was tested to be the fastest with 10.000 runs
+    common_start = None
+    common_end = None
     if 'type' in date and 'iso-date' in date:
         for i, field in enumerate(FIELDS, 1):
             if date['type'] == i:
                 data[f'activity-date.{field}'] = date['iso-date']
+                if i <= 2:
+                    common_start = date['iso-date']
+                else:
+                    common_end = date['iso-date']
+
+        if common_start:
+            data['activity-date.common.start'] = common_start
+        if common_end:
+            data['activity-date.common.end'] = common_end
     return data
