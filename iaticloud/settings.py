2fc0df54c7a858e9ff450bf484dfdb0524f27aec
Sylvan Ridderinkhof <sylvanridderinkhof@gmail.com>
2023-02-21 11:53:02 +0100

    chore: change deployment from gunicorn to nginx only

--- iaticloud/settings.py
+++ iaticloud/settings.py
@@ -27,7 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-f7*witax@a(&7p!zg_b_ougjt&3zsq&zjh@)c6)0%54naxd*6t')
 
-ALLOWED_HOSTS = []
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split()
 
 
