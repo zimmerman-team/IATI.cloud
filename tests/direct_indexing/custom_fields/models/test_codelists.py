6615c5a82e59742ec454124b818350a8ef4eeddc
Sylvan Ridderinkhof <sylvanridderinkhof@gmail.com>
2023-11-08 16:52:55 +0100

    refactor: extract file name in codelists test

--- tests/direct_indexing/custom_fields/models/test_codelists.py
+++ tests/direct_indexing/custom_fields/models/test_codelists.py
@@ -4,22 +4,24 @@ import pytest
 
 from direct_indexing.custom_fields.models.codelists import SOURCES, Codelists
 
+FILE_NAME = 'codelists.json'
+
 
 def test_codelist(mocker, tmp_path, requests_mock, fixture_codelists):
     # INTEGRATION
     # Test loading the existing codelists
     # write fixture_codelists to tmp_path/codelists.json
-    with open(tmp_path / 'codelists.json', 'w') as file:
+    with open(tmp_path / FILE_NAME, 'w') as file:
         json.dump(fixture_codelists, file)
     # mock settings.CODELISTS_JSON to tmp_path/codelists.json
-    mocker.patch('direct_indexing.custom_fields.models.codelists.settings.CODELISTS_JSON', tmp_path / 'codelists.json')
+    mocker.patch('direct_indexing.custom_fields.models.codelists.settings.CODELISTS_JSON', tmp_path / FILE_NAME)
     cl = Codelists(download=False)
     # Assert the codelists_dict is the same as fixture_codelists
     assert cl.get_codelists() == fixture_codelists
 
     # Test downloading the codelists
     # Remove the codelists.json file
-    (tmp_path / 'codelists.json').unlink()
+    (tmp_path / FILE_NAME).unlink()
     # mock the requests.get function to return fixture_codelists
     for k, v in SOURCES.items():
         res = [] if k not in fixture_codelists else fixture_codelists[k]
@@ -29,14 +31,14 @@ def test_codelist(mocker, tmp_path, requests_mock, fixture_codelists):
     for key in fixture_codelists:
         assert cl.codelists_dict[key] == fixture_codelists[key]
     # assert the file was re-created @ tmp_path/codelists.json
-    assert (tmp_path / 'codelists.json').exists()
+    assert (tmp_path / FILE_NAME).exists()
 
 
 def test_get_value(mocker, tmp_path, fixture_codelists):
-    with open(tmp_path / 'codelists.json', 'w') as file:
+    with open(tmp_path / FILE_NAME, 'w') as file:
         json.dump(fixture_codelists, file)
     # mock settings.CODELISTS_JSON to tmp_path/codelists.json
-    mocker.patch('direct_indexing.custom_fields.models.codelists.settings.CODELISTS_JSON', tmp_path / 'codelists.json')
+    mocker.patch('direct_indexing.custom_fields.models.codelists.settings.CODELISTS_JSON', tmp_path / FILE_NAME)
     cl = Codelists(download=False)
 
     # Test getting a non-existant codelist results in an empty list
