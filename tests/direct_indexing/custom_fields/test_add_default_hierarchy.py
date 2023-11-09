45906f214c1bc8df9faee466be4073bd20b573cb
Sylvan Ridderinkhof <sylvanridderinkhof@gmail.com>
2023-11-09 16:04:18 +0100

    feat: added tests for hierarchy

--- tests/direct_indexing/custom_fields/test_add_default_hierarchy.py
+++ tests/direct_indexing/custom_fields/test_add_default_hierarchy.py
@@ -1,5 +1,12 @@
-# TODO
+from direct_indexing.custom_fields.add_default_hierarchy import add_default_hierarchy
 
 
 def test_add_default_hierarchy():
-    pass
+    default_res = {'hierarchy': 1}
+    data = {}
+    add_default_hierarchy(data)
+    assert data == default_res
+
+    data = {'hierarchy': 2}
+    add_default_hierarchy(data)
+    assert data != default_res
