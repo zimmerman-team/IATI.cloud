264a6ef0dfe387faaf24f8dbad1e0372cde5f8de
Sylvan Ridderinkhof <sylvanridderinkhof@gmail.com>
2024-02-02 15:12:12 +0100

    chore: update IATI Data Dump url

--- iaticloud/settings.py
+++ iaticloud/settings.py
@@ -191,7 +191,7 @@ SOLR_ORGANISATION_URL = f'{SOLR_ORGANISATION}/update'
 # # IATI Data
 METADATA_PUBLISHER_URL = 'https://registry.codeforiati.org/publisher_list.json'
 METADATA_DATASET_URL = 'https://registry.codeforiati.org/dataset_list.json'
-DATASET_URL = 'https://gitlab.com/codeforIATI/iati-data/-/archive/main/iati-data-main.zip'
+DATASET_URL = 'https://data.iati-data-dump.opendataservices.coop/data.zip'
 
 # # PATHS
 SOLR_POST_TOOL = os.path.join(BASE_DIR, 'direct_indexing/solr/bin/post')
