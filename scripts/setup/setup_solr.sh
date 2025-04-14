435357a7036a02ace9946b6af1020b955d32e021
Sylvan Ridderinkhof <sylvanridderinkhof@gmail.com>
2025-04-14 13:49:20 +0200

    chore: keep solr_data folder tag and keep setup_solr to only the initual setup to keep the schemas flexible

--- scripts/setup/setup_solr.sh
+++ scripts/setup/setup_solr.sh
@@ -13,18 +13,9 @@ $1
 
 print_status "Setting up Solr..."
 
-# Ask the user if this is mounted locally, default to no. If it is, chown the files to 1001:root
-if ask_for_confirmation "Will the files be locally mounted (f.ex. on extra mounted volume)? You will be asked for the directory, where a ./solr directory will be created."; then
-  df -h
-  read -p "Enter your mounted directory: " mounted_dir
-  sudo mkdir -p $mounted_dir/solr
-  sudo chown -R 1001:root $mounted_dir/solr
-else
-  echo "Skipping mounted solr directory."
-fi
-
 sudo docker pull bitnami/solr:9.8.1
 sudo docker compose up solr -d
+print_status "Starting Solr for the first time, generating all relevant files..."
 sleep 60
 sudo bash ./direct_indexing/solr/update_solr_cores.sh
 sudo docker compose down
