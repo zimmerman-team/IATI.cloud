c3acabdb6c693bd7e33dbda12136f7b80a460dcc
Sylvan Ridderinkhof <sylvanridderinkhof@gmail.com>
2025-04-23 15:41:50 +0200

    chore: updated helper scripts

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,8 +1,8 @@
 echo "Restarting IATI.cloud (iaticloud, flower, workers, scheduler and revoke)"
 echo "Shutting down"
-sudo docker compose down iaticloud celeryflower celeryworker celeryscheduler celeryrevokeworker
+sudo docker stop iaticloud celeryflower celeryworker celeryscheduler celeryrevokeworker celeryaidaworker
 echo "Starting up..."
-sudo docker compose up -d iaticloud celeryflower celeryworker celeryscheduler celeryrevokeworker
+sudo docker compose up -d iaticloud celeryflower celeryworker celeryscheduler celeryrevokeworker celeryaidaworker
 echo "..."
 sleep 5
 echo "..."
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -8,6 +8,19 @@ if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
   exit 0
 fi
 
+# Check if the first argument is "all" or "ic"
+if [ "$1" = "all" ]; then
+  echo "Stopping all services..."
+  sudo docker compose down
+  echo "Stopping all services is done."
+  exit 0
+elif [ "$1" = "ic" ]; then
+  echo "Stopping IATI.cloud services (iaticloud celeryworker celeryaidaworker celeryrevokeworker celeryscheduler celeryflower)..."
+  sudo docker compose down iaticloud celeryworker celeryaidaworker celeryrevokeworker celeryscheduler celeryflower
+  echo "Stopping IATI.cloud services is done."
+  exit 0
+fi
+
 # Start
 # Get additional arguments (service names)
 I1="$1"
@@ -20,6 +33,7 @@ I7="$7"
 I8="$8"
 I9="$9"
 I10="$10"
+echo "Stopping services: $I1 $I2 $I3 $I4 $I5 $I6 $I7 $I8 $I9 $I10"
 sudo docker compose down $I1 $I2 $I3 $I4 $I5 $I6 $I7 $I8 $I9 $I10
 
 echo "Stop script is done."
