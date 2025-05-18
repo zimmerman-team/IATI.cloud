215a4693e3cc9dbfcf205c78cbd90fe6a2c5e52a
Sylvan Ridderinkhof <sylvanridderinkhof@gmail.com>
2025-05-18 16:26:41 +0200

    chore: update reset_solr script to target .env SOLV_VOLUME correctly

--- scripts/reset_solr.sh
+++ scripts/reset_solr.sh
@@ -2,7 +2,8 @@
 
 # script to reset solr
 sudo docker compose down
-sudo rm -rf ./direct_indexing/solr_data
+env_dir=$(grep -E '^SOLR_VOLUME=' .env | cut -d '=' -f2 | tr -d '"')
+sudo rm -rf $env_dir
 sudo bash scripts/setup/setup_solr_mount_dir.sh
 sudo bash scripts/setup/setup_solr.sh
 sudo docker compose down
